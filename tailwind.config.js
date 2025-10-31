6b05cd1ed48de8d58c9fd5a8e7baa019d0a8b87a
prodesignsbyanil-alt <prodesignsbyanil@gmail.com>
2025-10-31 12:50:02 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}"
+  ],
   darkMode: "class",
   theme: { extend: {} },
-  plugins: []
-};
+  plugins: [],
+}
